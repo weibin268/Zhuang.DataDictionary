f70bc64234bf58e97b7e13020247e538b2334524
zhuangweibin <binweizhuang@hotmail.com>
2016-08-21 21:25:08 +0800

    aaa

--- Zhuang.DataDictionary/Models/SysDataDictionary.cs
+++ Zhuang.DataDictionary/Models/SysDataDictionary.cs
@@ -8,6 +8,13 @@ namespace Zhuang.DataDictionary.Models
     [Zhuang.Data.Annotations.Table("Sys_DataDictionary")]
     public class SysDataDictionary
     {
+        [Zhuang.Data.Annotations.Key]
+        public string DataDictionaryId { get; set; }
 
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
--- Zhuang.DataDictionary/Models/SysDataDictionaryDetail.cs
+++ Zhuang.DataDictionary/Models/SysDataDictionaryDetail.cs
@@ -8,6 +8,19 @@ namespace Zhuang.DataDictionary.Models
     [Zhuang.Data.Annotations.Table("Sys_DataDictionaryDetail")]
     public class SysDataDictionaryDetail
     {
+        [Zhuang.Data.Annotations.Key]
+        public string DataDictionaryDetailId { get; set; }
 
+        public string DataDictionaryId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Text { get; set; }
+
+        public int Status { get; set; }
+
+        public int Seq { get; set; }
+
+        public string Description { get; set; }
     }
 }
