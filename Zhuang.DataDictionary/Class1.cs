c08a9c080ad3c628de55f8763778d92e6faa9987
zhuangweibin <binweizhuang@hotmail.com>
2016-08-21 21:19:22 +0800

    aaa

---
+++ Zhuang.DataDictionary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.DataDictionary
+{
+    public class Class1
+    {
+    }
+}
---
+++ Zhuang.DataDictionary/Models/SysDataDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.DataDictionary.Models
+{
+    [Zhuang.Data.Annotations.Table("Sys_DataDictionary")]
+    public class SysDataDictionary
+    {
+
+    }
+}
