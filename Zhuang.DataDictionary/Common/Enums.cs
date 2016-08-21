cf99335036112fd430aaf37018c906424689238d
zhuangweibin <binweizhuang@hotmail.com>
2016-08-21 21:37:50 +0800

    aaa

---
+++ Zhuang.DataDictionary/Common/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.DataDictionary.Common
+{
+    enum StatusType
+    {
+        Deleted = -1,
+        Disabled = 0,
+        Enabled = 1,
+    }
+}
--- Zhuang.DataDictionary/DataDictionaryManager.cs
+++ Zhuang.DataDictionary/DataDictionaryManager.cs
@@ -5,7 +5,8 @@ using System.Text;
 
 namespace Zhuang.DataDictionary
 {
-    public class Class1
+    public class DataDictionaryManager
     {
+
     }
 }
